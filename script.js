ce77aecbaacd672b2c9d278fafe53f1cd4377732
Igor <megakill123@yandex.ru>
2022-12-22 19:49:46 +0500

    edit project

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 const squareContainer = document.querySelector(".c-container");
 const squares = squareContainer.querySelectorAll(".square");
-const orderSquares = [];
-const tempsquares  = []
-
+let orderSquares = [];
 function addGreenSquare(square) {
   square.classList.add("square_color_green");
   orderSquares.push(square);
+  console.log(orderSquares.length)
 }
-let count = 0;
-squares.forEach((square) => {
-  square.addEventListener("click", () => {
-    if (!square.classList.contains("square_color_green")) {
-      square.classList.add("square_color_green");
-      orderSquares.push(square);
 
-      console.log(orderSquares.length);
-    }
-    if (orderSquares.length === 7) {
-      for (let i = 0; i <= orderSquares.length - 1; i++) {
-        (function (i) {
-          setTimeout(function () {
-            orderSquares[i].classList.remove("square_color_green");
-          }, 500 * (i + 1));
-        })(i);
+function removeGreenSquare(square) {
+  square.classList.remove("square_color_green");
+}
+
+function startAnimation() {
+  for (let i = 0; i <= orderSquares.length - 1; i++) {
+    setTimeout(function () {
+      removeGreenSquare(orderSquares[i]);
+    }, 500 * (i + 1));
+  }
+}
+
+squares.forEach((square) => {
+  square.addEventListener(
+    "click",
+    () => {
+      if (!square.classList.contains("square_color_green") && orderSquares.length !==7) {
+        addGreenSquare(square);
+      }
+      if (orderSquares.length === squares.length) {
+         startAnimation()
       }
     }
-  });
+  );
 });
